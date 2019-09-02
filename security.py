c5d5cc3a3ee935a311b465ff21fbca6ece6d9ff7
Replay <1575840203@qq.com>
2019-09-02 19:31:55 +0800

    Update security.py

--- security.py
+++ security.py
@@ -2,5 +2,5 @@
 import pymysql
 mysql_url = "mysql+pymysql://python:CCsys333!@10.47.0.115:3306/zabbix"
 
-db = pymysql.connect("10.47.0.115","python","CCsys333!","zabbix")
-db2 = pymysql.connect("10.47.0.115","python","CCsys333!","monitor")
+db = pymysql.connect("10.47.0.115","python","*****","zabbix")
+db2 = pymysql.connect("10.47.0.115","python","*****","monitor")
